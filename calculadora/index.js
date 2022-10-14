190e94b32ed3e1a7ec67f49c3edcaf70c0701367
eduarduuoliveira <106837258+eduarduuoliveira@users.noreply.github.com>
2022-10-14 15:07:57 -0300

    Add files via upload

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,44 +1,45 @@
-let resultado = document.getElementById('Resultado');
+let resultado = document.getElementById('resultado');
 let valor_salvo = null;
 let operador_salvo = null;
 
 let btnNumeros = document.getElementsByClassName('botao-numero')
-for (let botao of btnNumeros){
+for (let botao of btnNumeros) {
     botao.addEventListener('click', clica_numero);
 }
 
-function clica_numero(event){
-    if(isNaN(resultado.innerHTML) === false){
+function clica_numero(event) {
+    if (isNaN(resultado.innerHTML) === false) {
         resultado.innerHTML = resultado.innerHTML + event.target.innerHTML;
+    }
+
+    else {
+        resultado.innerHTML = event.target.innerHTML;
+    }
 }
 
-else{
-    resultado.innerHTML = event.target.innerHTML;
-}}
-
 let btnOperadores = document.getElementsByClassName('botao-operador');
-for(let botao of btnOperadores){
+for (let botao of btnOperadores) {
     botao.addEventListener("click", clica_operador);
 }
 
 function clica_operador(event) {
-    if (isNaN(resultado.innerHTML) === false){
-        if(valor_salvo === null){
+    if (isNaN(resultado.innerHTML) === false) {
+        if (valor_salvo === null) {
             valor_salvo = Number(resultado.innerHTML);
         }
-        else{
+        else {
             valor_salvo = executa_operacao(valor_salvo, operador_salvo, Number(resultado.innerHTML));
         }
-    
-}
 
-operador_salvo = event.target.innerHTML;
-resultado.innerHTML = event.target.innerHTML;
+    }
+
+    operador_salvo = event.target.innerHTML;
+    resultado.innerHTML = event.target.innerHTML;
 }
 
 let botaoC = document.getElementById('botao-c');
 botaoC.addEventListener("click", limpa_resultado);
-function limpa_resultado(event){
+function limpa_resultado(event) {
     resultado.innerHTML = "";
     valor_salvo = null;
     operador_salvo = null;
@@ -47,23 +48,25 @@ function limpa_resultado(event){
 let botaoIgual = document.getElementById('botao-igual');
 botaoIgual.addEventListener('click', calcula_resultado);
 
-function calcula_resultado (event){
-    if(valor_salvo != null && operador_salvo != null && isNaN(visor.innerHTML) === false){(valor_salvo, operador_salvo, Number(visor.innerHTML));
-    valor_salvo = null;
-    operador_salvo = null;
-}}
+function calcula_resultado(event) {
+    if (valor_salvo != null && operador_salvo != null && isNaN(resultado.innerHTML) === false) {
+        resultado.innerHTML = executa_operacao(valor_salvo, operador_salvo, Number(resultado.innerHTML));
+        valor_salvo = null;
+        operador_salvo = null;
+    }
+}
 
-function executa_operacao(valor1, operador, valor2){
-    if(operador === '+'){
+function executa_operacao(valor1, operador, valor2) {
+    if (operador === '+') {
         return valor1 + valor2;
     }
-    else if (operador === '-'){
+    else if (operador === '-') {
         return valor1 - valor2;
     }
-    else if (operador === '*'){
+    else if (operador === '*') {
         return valor1 * valor2;
     }
-    else if (operador === '/'){
+    else if (operador === '/') {
         return valor1 / valor2;
     }
 }
@@ -72,9 +75,18 @@ let body = document.getElementsByTagName('body')[0];
 
 body.addEventListener('keydown', pressionou_tecla);
 
-function pressionou_tecla(event){
-    if(event.key === "*"){
+function pressionou_tecla(event) {
+    if (event.key === "*") {
         document.getElementById('botao-multiplicacao').click();
     }
 }
 
+let botaoPonto = document.getElementById('botao-ponto');
+botaoPonto.addEventListener('click', clica_ponto);
+
+function clica_ponto(event) {
+    if (isNaN(resultado.innerHTML) === true)
+    resultado.innerHTML = ".";
+    else if (isNaN(resultado.innerHTML + '.') === false)
+    resultado.innerHTML = resultado.innerHTML + '.';
+}
